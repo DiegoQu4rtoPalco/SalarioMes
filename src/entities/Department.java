e7f14398fef8dcfd2a3d679e64844c67ed9baa8d
Diego Santana <diego_4palco@hotmail.com>
2019-06-20 17:59:42 -0300

    Encapsulamento String name

--- src/entities/Department.java
+++ src/entities/Department.java
@@ -1,5 +1,24 @@
 package entities;
 
 public class Department {
+	
+	private String name;
+	
+	//Cosntrutor vazio padrão.
+	public Department() {
+		
+	}
+	
+	//Construtor com argumentos.
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		
+	}
 
 }

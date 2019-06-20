39e1bd6b9d9b16d2ba756614f43028d9e1acaba4
Diego Santana <diego_4palco@hotmail.com>
2019-06-20 18:21:26 -0300

    Adicionado classe Worker em entities

---
+++ src/entities/Worker.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.util.List;
+
+public class Worker {
+	
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+	
+	private Department department;
+	private List<HoursContract> contracts;
+
+}

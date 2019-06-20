73e7edba5b8c0673e56e8ae1c985866195d33696
Diego Santana <diego_4palco@hotmail.com>
2019-06-20 17:53:08 -0300

    Adicionando classe Department no pacote entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,16 @@
+/*
+ * Ler os dados de um trabalhador com N contratos (N fornecido pelo 
+ * usuário). Depois, solicitar do usuário um mês e mostrar qual foi o 
+ * salário do funcionário nesse mês, conforme exemplo
+ * (próxima página).
+ */
 package application;
 
 public class Program {
+	
+	public static void main (String [] args) {
+		
+		
+	}
 
 }
